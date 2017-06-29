7c8dfbecb4bc63f8fb44edcca829834646f96aa6
zulkarnay <bedelbaev@gmail.com>
2017-06-29 15:24:25 +0600

    Add message in case of invalid command

--- bin/main.rb
+++ bin/main.rb
@@ -78,6 +78,12 @@ Telegram::Bot::Client.run(config.token) do |bot|
           reply_markup: Menu.complete
         )
       end
+    else
+      bot.api.send_message(
+        chat_id: message.chat.id,
+        text: 'Используйте команды из меню ниже.',
+        reply_markup: Menu.start
+      )
     end
   end
 end
